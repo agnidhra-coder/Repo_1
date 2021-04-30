28d2434c8e56935646ecc06df617a458cbed769f
RougherO <chiragchowdhury51@gmail.com>
2021-04-30 19:51:45 +0530

    Program is Ready

---
+++ pendulum_runner.py
@@ -0,0 +1,58 @@
+try:
+    import pygame
+    import pendulum_movement as pm
+    from math import pi, radians
+except Exception:
+    print("You are either missing one or more files or they are not in same directory")
+
+pygame.init()
+a1, a2 = 1440, 1280                     #Provide the diplay configuration here
+x_offset, y_offset = a1 // 2, a2 // 2
+p_c = dict()
+running = True
+
+
+def run(p):
+    pygame.draw.line(screen, (255, 255, 255), (x_offset, y_offset), (x_offset + (x1:=p.movement()[0]), y_offset - (y1:=p.movement()[1])))
+    pygame.draw.circle(screen, (255, 255, 255), (x_offset + x1, y_offset - y1), p.mass * 3)
+
+try:
+        
+    n = int(input("number of pendulums: "))
+
+    for i in range(n):
+        print(f"\nFor pendulum {i + 1}\n")
+        theta = radians(float(input("enter start angle[in degrees]: ")))
+        mass = float(input("enter mass[in kg and try to keep it small within 5]: "))
+        length = float(input("enter length[in m and same thing try keeping small < 3.5]: ")) * 100
+        p_c[i] = pm.pendulum(length, mass, theta)
+    print("to quit just close the window or press ESC.")
+
+    screen = pygame.display.set_mode([a1, a2])
+    
+    while running:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.display.quit()
+                pygame.quit()
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.display.quit()
+                    pygame.quit()
+                    running = False
+        screen.fill((0, 0, 0))
+        pygame.display.set_caption('Simple Pendulum')
+        for i in p_c:
+            run(p_c[i])
+        pygame.display.update()
+        pygame.time.delay(13)
+
+    print("Thank You for trying out my Simple Pendulum project... Will upload double pendulum too so stay tuned")
+except Exception as e:
+        print(e)
+        pygame.display.quit()
+        pygame.quit()
+        running = False    
+
